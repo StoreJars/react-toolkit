7fdad3dc7f35c909d985f1cd9bfec4f2ce9acd86
Victor Akerele <eneroakerele@gmail.com>
2020-03-27 11:59:14 +0100

    exported actions, selctors and metas

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
 export { configureStore, rootEpic, epicMiddleware } from './redux/store';
 
+export { action as admins, selector as adminsSelector, metaSelector as adminsMetaSelector } from './redux/entities/admins';
+export { action as businessTypes, selector as businessTypesSelector, metaSelector as businessTypesMetaSelector } from './redux/entities/businessTypes';
+export { action as carts, selector as cartsSelector, metaSelector as cartsMetaSelector } from './redux/entities/carts';
+export { action as categories, selector as categoriesSelector, metaSelector as categoriesMetaSelector } from './redux/entities/categories';
+export { action as customers, selector as customersSelector, metaSelector as customersMetaSelector } from './redux/entities/customers';
+export { action as domains, selector as domainsSelector, metaSelector as domainsMetaSelector } from './redux/entities/domains';
+export { action as login, selector as loginSelector, metaSelector as loginMetaSelector } from './redux/entities/login';
+export { action as orders, selector as ordersSelector, metaSelector as ordersMetaSelector } from './redux/entities/orders';
+export { action as permissions, selector as permissionsSelector, metaSelector as permissionsMetaSelector } from './redux/entities/permissions';
+export { action as posts, selector as postsSelector, metaSelector as postsMetaSelector } from './redux/entities/posts';
+export { action as products, selector as productsSelector, metaSelector as productsMetaSelector } from './redux/entities/products';
+export { action as register, selector as registerSelector, metaSelector as registerMetaSelector } from './redux/entities/register';
+export { action as resources, selector as resourcesSelector, metaSelector as resourcesMetaSelector } from './redux/entities/resources';
+export { action as roles, selector as rolesSelector, metaSelector as rolesMetaSelector } from './redux/entities/roles';
+export { action as store, selector as storeSelector, metaSelector as storeMetaSelector } from './redux/entities/store';
+export { action as supscriptionPlans, selector as supscriptionPlansSelector, metaSelector as supscriptionPlansMetaSelector } from './redux/entities/supscriptionPlans';
+export { action as tags, selector as tagsSelector, metaSelector as tagsMetaSelector } from './redux/entities/tags';
+export { action as themes, selector as themesSelector, metaSelector as themesMetaSelector } from './redux/entities/themes';
+export { action as vendors, selector as vendorsSelector, metaSelector as vendorsMetaSelector } from './redux/entities/vendors';
+export { action as verifyToken, selector as verifyTokenSelector, metaSelector as verifyTokenMetaSelector } from './redux/entities/verifyToken';
+
