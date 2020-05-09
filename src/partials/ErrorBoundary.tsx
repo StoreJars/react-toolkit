8f090c1567d0a2e78a1ac85f43ca30123379b0ab
Victor Akerele <eneroakerele@gmail.com>
2020-05-09 18:34:42 +0100

    added error boundary

---
+++ src/partials/ErrorBoundary.tsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+type props = {};
+type state = { hasError: boolean, error: string, info: any };
+
+export default class ErrorBoundary extends Component<props, state> {
+  // tslint:disable-next-line: member-access
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: '', info: '' };
+  }
+
+  // tslint:disable-next-line: member-access
+  componentDidCatch(error, info) {
+    this.setState({ error, info });
+    console.log(error);
+  }
+
+  // tslint:disable-next-line: member-access
+  render() {
+    const { hasError, error, info } = this.state;
+    const { children } = this.props;
+
+    if (hasError) {
+      return (
+        <div style={{ padding: '20px' }}>
+          <h6 style={{ fontSize: '12px', color: 'red' }}>
+            Something went wrong,
+            {error.toString()}
+            {info.componentStack}
+          </h6>
+        </div>
+      );
+    }
+    return children;
+  }
+}
