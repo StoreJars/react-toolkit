07b80b71347b4c59cf884e8b7d89ce237877278b
Victor Akerele <eneroakerele@gmail.com>
2020-05-09 18:37:18 +0100

    updated up buttons and selectinputs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,4 @@
 export { default as countries } from './countries';
 export { default as states } from './states';
 
-const range = (start, end) => Array.from({ length: (end - start) }, (v, k) => k + start);
-
-export const years = range(1990, 2019).reverse();
+export const arrayFrom = (start, end) => Array.from({ length: (end - start) }, (v, k) => k + start);
