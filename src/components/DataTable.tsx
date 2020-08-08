70dfa06118df0b1fd76182f8d6ef1bd6a3f42cb7
Victor Akerele <eneroakerele@gmail.com>
2020-08-08 21:39:42 +0100

    merge with upstream

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,36 +1,59 @@
 import React from 'react';
-import ReactDatatable from '@ashvin27/react-datatable';
-
-const config = {
-  page_size: 10,
-  length_menu: [10, 20, 50],
-  button: {
-    excel: true,
-    print: true,
-    extra: true,
-  },
-};
-
-const extraButtons = [
-  // {
-  //   className: "btn btn-primary buttons-pdf",
-  //   title: "Export Test",
-  //   children: [
-  //     <span>
-  //       <i className="glyphicon glyphicon-print fa fa-print" aria-hidden="true"></i>
-  //     </span>
-  //   ],
-  //   onClick: (event) => { },
-  // },
-];
+import ReactDatatable from 'react-data-table-component';
 
 interface IProps {
-  records: any;
+  data: any;
   columns: any;
 }
 
 export default function DataTable(props: IProps) {
-  const { records, columns } = props;
+  const { data, columns } = props;
 
-  return <ReactDatatable config={config} records={records} columns={columns} extraButtons={extraButtons} />;
+  const [selectableRows, setSelectableRows] = React.useState(false);
+  const [noSelectAll, setNoSelectAll] = React.useState(false);
+  const [selectableRowsVisibleOnly, setSelectableRowsVisibleOnly] = React.useState(false);
+  const [selectableRowsHighlight, setSelectableRowsHighlight] = React.useState(false);
+  const [expandableRows, setExpandableRows] = React.useState(false);
+  const [expandOnRowClick, setExpandOnRowClick] = React.useState(false);
+  const [pagination, setPagination] = React.useState(true);
+  const [highlight, setHighlight] = React.useState(false);
+  const [striped, setStriped] = React.useState(false);
+  const [pointer, setPointer] = React.useState(false);
+  const [dense, setDense] = React.useState(false);
+  const [persist, setPersist] = React.useState(false);
+  const [tableHead, setNoHead] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [noHeader, setNoHeader] = React.useState(false);
+  const [subHeader, setSubHeader] = React.useState(false);
+  const [subHeaderAlign, setSubHeaderAlign] = React.useState('right');
+  const [fixedHeader, setFixedheader] = React.useState(false);
+
+  return (
+    <ReactDatatable
+      title="Movie List"
+      columns={columns}
+      data={data}
+      defaultSortField="title"
+      selectableRows={selectableRows}
+      selectableRowsNoSelectAll={noSelectAll}
+      selectableRowsHighlight={selectableRowsHighlight}
+      selectableRowsVisibleOnly={selectableRowsVisibleOnly}
+      expandableRows={expandableRows}
+      expandOnRowClicked={expandOnRowClick}
+      pagination={pagination}
+      highlightOnHover={highlight}
+      striped={striped}
+      pointerOnHover={pointer}
+      dense={dense}
+      noTableHead={tableHead}
+      persistTableHead={persist}
+      progressPending={loading}
+      noHeader={noHeader}
+      subHeader={subHeader}
+      subHeaderComponent={<div style={{ display: 'flex', alignItems: 'center' }} />}
+      subHeaderAlign={subHeaderAlign}
+      fixedHeader={fixedHeader}
+      fixedHeaderScrollHeight="300px"
+    />
+  );
 }
