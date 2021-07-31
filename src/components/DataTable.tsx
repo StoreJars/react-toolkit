d3b5cfe4c7de32c6f80c890b119ff02f643f7651
Victor Akerele <eneroakerele@gmail.com>
2021-07-31 10:21:15 +0100

    enable excel export support from datatable

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import ReactDatatable from 'storejars-react-datatable';
 
-const config = {
-  page_size: 50,
-  length_menu: [50, 100, 150, 200],
-  button: {
-    excel: false,
-    print: false,
-    extra: false,
-  },
-};
-
 const extraButtons = [];
 
 interface IProps {
   records: any;
   columns: any;
+  excel?: boolean;
+  print?: boolean;
 }
 
 export default function DataTable(props: IProps) {
-  const { records, columns } = props;
+  const { records, columns, excel, print } = props;
+
+  const config = {
+    page_size: 50,
+    length_menu: [50, 100, 150, 200],
+    button: {
+      excel,
+      print,
+      extra: false,
+    },
+  };
 
   return <ReactDatatable config={config} records={records} columns={columns} extraButtons={extraButtons} />;
 }
