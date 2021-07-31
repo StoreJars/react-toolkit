04d53a2c906d519b74d707a7bbe2548e67285f5a
Victor Akerele <eneroakerele@gmail.com>
2021-07-31 10:31:23 +0100

    csv export

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -8,17 +8,21 @@ interface IProps {
   columns: any;
   excel?: boolean;
   print?: boolean;
+  csv?: boolean;
+  filename?: string;
 }
 
 export default function DataTable(props: IProps) {
-  const { records, columns, excel, print } = props;
+  const { records, columns, csv, print, filename } = props;
 
   const config = {
     page_size: 50,
     length_menu: [50, 100, 150, 200],
+    filename,
     button: {
-      excel,
+      csv,
       print,
+      excel: false,
       extra: false,
     },
   };
